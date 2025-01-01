ad2fab09407ae73c42fc5bdcdcfa03da632d22e8
Sahil Dhingra <sahildhingra1812@gmail.com>
2025-01-01 14:06:06 +0530

    Initial commit with app.js and other files

--- app.js
+++ app.js
@@ -11,16 +11,19 @@ const connectDB = require('./server/config/db');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// Serve static files
 app.use(express.static('public'));
 
-// Connect to DB
+// Connect to the database
 connectDB();
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 
+// Session configuration with MongoDB
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
@@ -30,12 +33,12 @@ app.use(session({
     }),
 }));
 
-// Templating Engine
+// EJS Templating Engine setup
 app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-// Mount routes
+// Define routes
 app.use('/', require('./server/routes/main'));
 app.use('/', require('./server/routes/admin'));
 
@@ -54,55 +57,11 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something went wrong!');
 });
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
 
 
-// require('dotenv').config();
-
-// const express = require('express');
-// const expressLayout = require('express-ejs-layouts');
-// const cookieParser = require('cookie-parser');
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo');
-// const methodOverride = require('method-override');
-
-
-// const connectDB = require('./server/config/db');
-
-// const app = express();
-// const PORT = process.env.PORT || 5001;
-
-// app.use(express.static('public'));
-
-// //connect to DB
-// connectDB();
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(methodOverride('_method'));
-
-// app.use(session({
-//     secret: 'keyboard cat',
-//     resave: false,
-//     saveUninitialized: true,
-//     store: MongoStore.create({
-//     mongoUrl: process.env.MONGODB_URI
-//     }),
-//     //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
-// }));
-
-// // Templating Engine
-// app.use(expressLayout);
-// app.set('layout', './layouts/main');
-// app.set('view engine', 'ejs');
-
-// app.use('/', require('./server/routes/main'));
-// app.use('/', require('./server/routes/admin'));
-
-// app.listen(PORT, ()=> {
-//     console.log(`App listening on port ${PORT}`);
-// });
+// For Vercel to properly handle serverless function deployment, use this
+module.exports = app;
